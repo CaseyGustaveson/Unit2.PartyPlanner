f591e48b0ffb4cfa16f63bfd43b636d578da3caa
Casey Gustaveson <gustcase@isu.edu>
2024-04-17 20:33:52 -0500

    fix

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const COHORT = "REPLACE_ME!";
+const COHORT = "2109-CPU-RM-WEB-PT";
 const API_URL = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/${COHORT}/artists`;
 
 const state = {
@@ -23,22 +23,63 @@ render();
  * Update state with artists from API
  */
 async function getArtists() {
-  // TODO
+  try {
+    const response = await fetch(API_URL);
+    const data = await response.json();
+    state.artists = data.data;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 /**
  * Render artists from state
  */
 function renderArtists() {
-  // TODO
+  for (let i = 0; i < state.artists.length; i++) {
+    const currentArtist = state.artists[i];
+    const myDiv = document.createElement("div");
+    myDiv.innerHTML = `
+        <img src=${currentArtist.imageUrl}/>
+        <p>${currentArtist.name}</p>
+        <p>${currentArtist.description}</p>
+        `;
+    artistList.appendChild(myDiv);
+  }
 }
 
 /**
  * Ask the API to create a new artist based on form data
  * @param {Event} event
  */
-function addArtist(event) {
+async function addArtist(event) {
   event.preventDefault();
 
-  // TODO
+  const nameInput = document.querySelector('input[name="name"]')
+  const descriptionInput = document.querySelector('input[name="description"]')
+  const imageUrlInput = document.querySelector('input[name="imageUrl"]')
+  
+  const name = nameInput.value
+  const description = descriptionInput.value;
+  const imageUrl = imageUrlInput.value
+
+  try {
+    const response = await fetch(API_URL, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        name,
+        imageUrl,
+        description
+      }),
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to create artist");
+    }
+
+    render();
+  } catch (error) {
+    console.error(error);
+  }
 }
