ae8416135076cff0aa8f4e83b0b5361093b620c4
CaseyGustaveson <161667272+CaseyGustaveson@users.noreply.github.com>
2024-04-18 14:10:13 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,85 +1,65 @@
 const COHORT = "2109-CPU-RM-WEB-PT";
-const API_URL = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/${COHORT}/artists`;
+const API_URL = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/${COHORT}/events`;
 
 const state = {
-  artists: [],
+    events: [],
 };
 
-const artistList = document.querySelector("#artists");
+const eventList = document.querySelector("#events");
 
-const addArtistForm = document.querySelector("#addArtist");
-addArtistForm.addEventListener("submit", addArtist);
+const addEventForm = document.querySelector("#addEvent");
+addEventForm.addEventListener("submit", addEvent);
 
-/**
- * Sync state with the API and rerender
- */
-async function render() {
-  await getArtists();
-  renderArtists();
-}
-render();
 
 /**
- * Update state with artists from API
+ * Update state with events from API
  */
-async function getArtists() {
-  try {
-    const response = await fetch(API_URL);
-    const data = await response.json();
-    state.artists = data.data;
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-/**
- * Render artists from state
- */
-function renderArtists() {
-  for (let i = 0; i < state.artists.length; i++) {
-    const currentArtist = state.artists[i];
-    const myDiv = document.createElement("div");
-    myDiv.innerHTML = `
-        <img src=${currentArtist.imageUrl}/>
-        <p>${currentArtist.name}</p>
-        <p>${currentArtist.description}</p>
-        `;
-    artistList.appendChild(myDiv);
-  }
-}
-
-/**
- * Ask the API to create a new artist based on form data
- * @param {Event} event
- */
-async function addArtist(event) {
-  event.preventDefault();
-
-  const nameInput = document.querySelector('input[name="name"]')
-  const descriptionInput = document.querySelector('input[name="description"]')
-  const imageUrlInput = document.querySelector('input[name="imageUrl"]')
-  
-  const name = nameInput.value
-  const description = descriptionInput.value;
-  const imageUrl = imageUrlInput.value
-
-  try {
-    const response = await fetch(API_URL, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        name,
-        imageUrl,
-        description
-      }),
-    });
-
-    if (!response.ok) {
-      throw new Error("Failed to create artist");
+async function getEvents() {
+    try {
+        const response = await fetch(API_URL);
+        const data = await response.json();
+        state.events = data.data;
+    } catch (error) {
+        console.log(error);
     }
+}
 
-    render();
-  } catch (error) {
-    console.error(error);
-  }
+
+/**
+ * Ask the API to create a new event based on form data
+ */
+async function addEvent(event) {
+    event.preventDefault();
+
+    const nameInput = document.querySelector('input[name="name"]')
+    const dateInput = document.querySelector('input[name="date"]')
+    const timeInput = document.querySelector('input[name="time"]')
+    const locationInput = document.querySelector('input[name="location"]')
+    const descriptionInput = document.querySelector('input[name="description"]')
+
+    const name = nameInput.value;
+    const date = dateInput.value;
+    const time = timeInput.value;
+    const location = locationInput.value
+    const description = descriptionInput.value;
+
+    try {
+        const response = await fetch(API_URL, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                name,
+                date,
+                time,
+                location,
+                description
+            }),
+        });
+
+        if (!response.ok) {
+            throw new Error("Failed to create event");
+        }
+    } catch (error) {
+        console.error(error);
+    }
 }
